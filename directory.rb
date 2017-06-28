ff48d4ebeea6d7e66a65ecedcaea505df89b20bf
marcusfgardiner <marcusfgardiner@gmail.com>
2017-06-28 11:12:48 +0000

    Step 12 complete - can load data from students csv file. My code has more functionality than the makers one, so additional updates that would improve code are (1) making conditions and typos menu options and (2) storing and loading all other variables I created, not jsut name and cohort

--- directory.rb
+++ directory.rb
@@ -17,11 +17,21 @@ def save_students
     file.close
 end
 
+def load_students
+    file = File.open("students.csv", "r")
+    file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+        @students << {name: name, cohort: cohort.to_sym}
+    end
+    file.close
+end
+
 def print_menu
     #1. Print the menu and ask what the user wants to do
     puts "1. Input the students"
     puts "2. Show the students"
     puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
     puts "9. Exit" #9 as more items to come
 end
 
@@ -33,6 +43,8 @@ def process(selection)
             print_students_list
         when "3"
             save_students
+        when "4"
+            load_students
         when "9"
             exit #terminates program
         else
